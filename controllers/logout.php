a8b5f5ee601a9b1bfdec5cc5a194471ad4ac64f0
juancho <juroscachaya@gmail.com>
2026-02-26 01:07:15 -0500

    5

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,12 +1,7 @@
-
 <?php
-require_once '../config/database.php';
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
 session_destroy();
 header('Location: ../views/auth/login.php?success=Sesión cerrada exitosamente');
 exit;
-?>
