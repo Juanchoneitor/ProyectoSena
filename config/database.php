f1932ea7986f3f91d7ab3b0f74b21f78ef560c1a
juancho <juroscachaya@gmail.com>
2026-02-26 00:29:41 -0500

    2

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
 $host     = getenv('DB_HOST')     ?: 'localhost';
+$port     = getenv('DB_PORT')     ?: '3306';
 $db_name  = getenv('DB_NAME')     ?: 'cursos_app';
 $username = getenv('DB_USER')     ?: 'root';
 $password = getenv('DB_PASSWORD') ?: '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db_name;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
